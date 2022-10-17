6fdcb889874a77660dd0ada83b7e612c849e72ee
Kevin Sasse <sassek70@gmail.com>
2022-10-17 11:58:46 -0400

    finished lab my method

--- src/components/App.js
+++ src/components/App.js
@@ -11,10 +11,15 @@ function App() {
     setIsDarkMode((isDarkMode) => !isDarkMode);
   }
 
+  const handleItemFormSubmit=(newItem)=>{
+    setItems([...items, newItem])
+  }
+  console.log(items)
+
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
       <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
-      <ShoppingList items={items} />
+      <ShoppingList items={items} onItemFormSubmit={handleItemFormSubmit}/>
     </div>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -3,25 +3,38 @@ import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
 
-function ShoppingList({ items }) {
+function ShoppingList({ items, onItemFormSubmit }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [searchedItem, setSearcheditem] = useState("")
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
+  
   const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
-
+    if (selectedCategory === "All") {
+     return true;
+    } else {    
     return item.category === selectedCategory;
+    }
   });
+  
+ const searchResults = itemsToDisplay.filter((item) => {
+    return item.name.toLowerCase().includes(searchedItem.toLowerCase())
+ })
+
+  function handleSearchChange(event) {
+    setSearcheditem(event.target.value)
+  }
+
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm onItemFormSubmit={onItemFormSubmit}/>
+      <Filter onCategoryChange={handleCategoryChange} onSearchChange={handleSearchChange} />
       <ul className="Items">
-        {itemsToDisplay.map((item) => (
+        {searchResults.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
